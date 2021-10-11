0609128241236c91d5033b8991f57c161fa9a66b
anais <anais.bruno26@gmail.com>
2021-10-11 20:42:18 +0200

    test2

--- src/main/java/app/MonMain.java
+++ src/main/java/app/MonMain.java
@@ -1,10 +1,15 @@
 package app;
 
+import model.Personne;
+
 public class MonMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Personne p = new Personne ("toto");
+		
+		
 	}
 
 }
--- src/main/java/model/Personne.java
+++ src/main/java/model/Personne.java
@@ -3,4 +3,13 @@ package model;
 public class Personne {
 
 	private String toto;
+
+	public Personne(String toto) {
+		super();
+		this.toto = toto;
+	}
+	
+	
+	
+	
 }
